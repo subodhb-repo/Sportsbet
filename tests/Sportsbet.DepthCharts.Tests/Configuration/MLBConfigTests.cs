3d60f56ed648860090f8834804f416e5f3f6d173
Subodh Bhandary <subodhb@outlook.com.au>
2025-11-05 09:36:26 +1100

    feat: add config tests

---
+++ tests/Sportsbet.DepthCharts.Tests/Configuration/MLBConfigTests.cs
@@ -0,0 +1,48 @@
+﻿using Sportsbet.DepthCharts.Domain.Configuration;
+
+namespace Sportsbet.DepthCharts.Tests.Configuration;
+
+public class MLBConfigTests
+{
+    [Fact]
+    public void IsValidPosition_ShouldReturnTrue_WhenPositionIsValid()
+    {
+        // Arrange
+        var validPositions = new[]
+        {
+            "SP",
+            "RP",
+            "C",
+            "1B",
+            "2B",
+            "3B",
+            "SS",
+            "LF",
+            "RF",
+            "CF",
+            "DH",
+        };
+
+        foreach (var position in validPositions)
+        {
+            // Act
+            var result = new MLBConfiguration().IsValidPosition(position);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("invalid")]
+    public void IsValidPosition_ShouldReturnFalse_WhenPositionIsInValid(string invalidPosition)
+    {
+        // Act
+        var result = new MLBConfiguration().IsValidPosition(invalidPosition);
+
+        // Assert
+        Assert.False(result);
+    }
+}
---
+++ tests/Sportsbet.DepthCharts.Tests/Configuration/NFLConfigTests.cs
@@ -0,0 +1,35 @@
+﻿using Sportsbet.DepthCharts.Domain.Configuration;
+
+namespace Sportsbet.DepthCharts.Tests.Configuration;
+
+public class NFLConfigTests
+{
+    [Fact]
+    public void IsValidPosition_ShouldReturnTrue_WhenPositionIsValid()
+    {
+        // Arrange
+        var validPositions = new[] { "QB", "WR", "RB", "TE", "K", "P", "KR", "PR" };
+
+        foreach (var position in validPositions)
+        {
+            // Act
+            var result = new NFLConfiguration().IsValidPosition(position);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("invalid")]
+    public void IsValidPosition_ShouldReturnFalse_WhenPositionIsInValid(string invalidPosition)
+    {
+        // Act
+        var result = new NFLConfiguration().IsValidPosition(invalidPosition);
+
+        // Assert
+        Assert.False(result);
+    }
+}
